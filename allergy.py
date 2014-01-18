ef02c93d8bf687e0c5dcfbb6507d6a5c26e6fc24
adamdaniel1 <adamdaniel124@gmail.com>
2014-01-18 18:32:30 -0500

    allergy

---
+++ allergy.py
@@ -0,0 +1,10 @@
+# This script will be the basic model of the allergy idea
+# Part I: 
+	# Show the user past files that have been created. If none, prompt the user to create one.
+
+import os
+filestuff = open("C:\\Users\\adamd_000\\Documents")
+filestuff.read()
+print filestuff
+
+filestuff.close()
