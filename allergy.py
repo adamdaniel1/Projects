4e9494f4b290419a7843cab90e32c3a06feff690
adamdaniel1 <adamdaniel124@gmail.com>
2014-01-18 19:24:14 -0500

    allergy changes

--- allergy.py
+++ allergy.py
@@ -3,8 +3,15 @@
 	# Show the user past files that have been created. If none, prompt the user to create one.
 
 import os
-filestuff = open("C:\\Users\\adamd_000\\Documents")
-filestuff.read()
-print filestuff
+userdir = raw_input("Please enter the directory name where you store your allergy information:\n\t>")
 
-filestuff.close()
+print ""
+print "This is a list of all the Directories and files... "
+filestuff = os.listdir(userdir)
+print filestuff
+print ""
+
+userfile1 = raw_input("Please choose which file you would like to open: ")
+userfile2 = open(userfile1,'r')
+print userfile2.read()
+
